7fe4ec364eab8bd0a900c1950e0c2b766e5f1b4d
Timothy Denning <timothydenning@Timothys-MacBook-Pro-2.local>
2022-03-03 10:57:05 +0100

    template imporvements

--- src/app/landing-page/section/section.component.ts
+++ src/app/landing-page/section/section.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { toHTML } from '@portabletext/to-html';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss']
 })
-export class SectionComponent implements OnInit {
+export class SectionComponent implements OnInit, OnChanges {
   @Input() callToAction!: {
     title: string;
     route: string;
@@ -13,12 +14,20 @@ export class SectionComponent implements OnInit {
   }
   @Input() title!: string;
   @Input() subtitle!: string;
-  @Input() description!: string;
+  @Input() description!: any;
   @Input() imageUrl!: string;
+
+  descriptionHTML!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.descriptionHTML = toHTML(changes['description'].currentValue);
+    console.log(this.descriptionHTML) 
   }
 
 }
